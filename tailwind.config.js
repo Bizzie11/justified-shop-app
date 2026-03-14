a88898db3981130b35bb66850de6a5648193e978
Bizzie11 <Support@justifiedventuresllc.com>
2026-03-14 18:03:55 -0400

    Fix merge conflict in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> 08b940da27fd93490575905771df0175d5cdb675
 };
