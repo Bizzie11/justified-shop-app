d384582ca4dbae6724e813947c1bf7a0b7524f7e
Bizzie11 <Support@justifiedventuresllc.com>
2026-03-19 09:20:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -271,7 +271,10 @@ let currentDate = storedDate;
       setLastSearchDate(today);
     }
  
-
+console.log("COUNTER DEBUG", {
+  termToSearch,
+  currentCount,
+});
   if (currentCount >= 5) {
   showToast("You’ve reached your 5 free searches for today. Upgrade to Pro for unlimited searches.");
   return;
